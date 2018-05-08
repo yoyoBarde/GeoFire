47140dfcd89b48e3afd8251b2424575a42fe67fe
Julius Barde <yam1434414344@gmail.com>
2018-05-08 08:21:30 -0700

    2nd commit

--- Desktop/MyApplication2/app/src/main/java/com/example/j/myapplication/MapsActivity.kt
+++ Desktop/MyApplication2/app/src/main/java/com/example/j/myapplication/MapsActivity.kt
@@ -1,7 +1,19 @@
 package com.example.j.myapplication
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GooglePlayServicesUtil
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -10,9 +22,28 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    override fun onConnectionFailed(p0: ConnectionResult) {
+    }
+
+    override fun onConnected(p0: Bundle?) {
+    }
+
+    override fun onConnectionSuspended(p0: Int) {
+    }
 
     private lateinit var mMap: GoogleMap
+    val MY_PERMISSION_REQUEST_CODE =7192
+    val PLAY_SERVICES_RESOLUTION_REQUEST=300193
+    lateinit var mLocationRequest: LocationRequest
+    lateinit var mGoogleClient: GoogleApiClient
+    lateinit var mLastLocation: Location
+    var TAG="YOYOBARDE"
+    var UPDATE_INTERVAL:Long=5000 //5secs
+    var FASTEST_INTERVAL:Long=3000
+    var DISPLACEMENT:Long=10
+    var PRIORITY_HIGH_ACCURACY = LocationRequest.PRIORITY_HIGH_ACCURACY
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,17 +52,82 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+    }
+    fun setUpLocation() {
+        val MPLIST = arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION)
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, MPLIST, MY_PERMISSION_REQUEST_CODE)
+
+        } else {
+            if (checkPlayServices()) {
+                buildGoogleAPIClient()
+                createLocationRequest()
+                displayLocation()
+            }
+
+        }
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    private fun displayLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            return
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleClient)
+        if(mLastLocation!=null) {
+            var latitude = mLastLocation.latitude
+            var longtitude = mLastLocation.longitude
+            Log.d(TAG,"Your Location was change")
+
+        }
+        else
+            Log.d(TAG,"Cannot get your location")
+    }
+
+    fun createLocationRequest() {
+      var mLocationRequest= mLocationRequest
+       mLocationRequest.setInterval(UPDATE_INTERVAL)
+       mLocationRequest.setFastestInterval(FASTEST_INTERVAL)
+       mLocationRequest.setPriority(PRIORITY_HIGH_ACCURACY)
+mLocationRequest.setSmallestDisplacement(DISPLACEMENT.toFloat())
+    }
+
+    fun buildGoogleAPIClient(){
+      var mGoogleApiClient =GoogleApiClient.Builder(this)
+              .addConnectionCallbacks(this)
+              .addOnConnectionFailedListener(this)
+              .addApi(LocationServices.API)
+              .build()
+                mGoogleApiClient.connect()
+   }
+
+    fun checkPlayServices(): Boolean{
+        var resultCode=GooglePlayServicesUtil.isGooglePlayServicesAvailable(this)
+       if (resultCode!= ConnectionResult.SUCCESS) {
+           if(GooglePlayServicesUtil.isUserRecoverableError(resultCode))
+           GooglePlayServicesUtil.getErrorDialog(resultCode, this, PLAY_SERVICES_RESOLUTION_REQUEST).show()
+
+           else{
+               Toast.makeText(this, "This device is not supported", Toast.LENGTH_SHORT).show()
+               finish()
+           }
+       return false
+       }
+       return true
+
+   }
+
+
+
+
+
+
+
+
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
@@ -41,3 +137,4 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
+    
